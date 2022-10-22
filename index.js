548d1a453c3eb54c90fa68d0e3446e54b8d82a59
Stanislav Prykhodko <stasprykhodko@gmail.com>
2022-10-22 03:46:49 +0300

    Made correction work

--- index.js
+++ index.js
@@ -2,23 +2,16 @@ window.onload = function(){
     let wrapper=document.querySelector(".wrapper")
     let counter=0
 
-
-
     setInterval(slide, 5000)
     function slide(){
-        // for (let radio of document.querySelectorAll('[type="radio"]')) {
-        //     radio.checked = false;
-        //   }   
 
         wrapper.style.left = `${counter*-100}%`;
         counter >= 2 ? counter = 0 : counter++ ? wrapper.style.transition="all linear 1s" : wrapper.style.transition="all linear 0.3s"
-        // console.log(counter)
     }
 
     let btn = document.querySelector("#asd")
     document.querySelector("#asd").addEventListener('click', offset)
     function offset(e){
-        // if(e.target.type === 'radio'){
             if (e.target.id === 'radioBtn1') {
                 counter = 0;
                 wrapper.style.left = `${counter*-100}%`
@@ -34,23 +27,7 @@ window.onload = function(){
                 wrapper.style.left = `${counter*-100}%`
                 wrapper.style.transition="all linear 1s"
             }
-        // }
     }
-
-    // radioBtn1
-    
-    // for (let radio of document.querySelectorAll('[type="radio"]')) {
-    //     console.log(radio)
-    //     if(document.querySelector(".headerBackgroundA")){
-    //         radio.checked = true;
-    //     }    
-    //     if(document.querySelector(".headerBackgroundB")){
-    //         radio.checked = true;
-    //     }  
-    //     if(document.querySelector(".headerBackgroundC")){
-    //         radio.checked = true;
-    //     }  
-    // }
 }
 
 
--- pizzasiteContact.js
+++ pizzasiteContact.js
@@ -4,12 +4,8 @@ console.log(form)
 form.addEventListener("submit", function (e) {
     if (form.name.value.length == 0 || form.surname.value.length == 0) {
         e.preventDefault()
-        // form.name.setCustomValidity("Введите имя пользователя.");
-        // form.surname.setCustomValidity("Введите фамиию пользователя.");
     }
     else{
-        // form.name.setCustomValidity("");
-        // form.surname.setCustomValidity("");
         alert("Форма отправлена");
     }
 
